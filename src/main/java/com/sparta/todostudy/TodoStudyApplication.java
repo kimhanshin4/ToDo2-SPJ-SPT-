db4b0cdd6e4e0e15d9564d4bd94c9bc7e4693370
kimhanshin4 <funold1004@gmail.com>
2023-11-20 23:27:12 +0900

    first commit

---
+++ src/main/java/com/sparta/todostudy/TodoStudyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/todostudy/TodoStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.todostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
